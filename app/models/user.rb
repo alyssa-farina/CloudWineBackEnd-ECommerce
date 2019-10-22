c34be90f94e7bb2c95342a03de46dcc4348c7543
Alyssa Farina <alyssafarina695@gmail.com>
2019-10-22 10:43:36 -0400

    final final touches

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
     has_many :events
     has_many :wineries
     has_secure_password
-    validates :username, uniqueness: {case_sensitive: false}
+    # validates :username, uniqueness: {case_sensitive: false}
 end
